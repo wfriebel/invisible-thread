9239b3b60018270ca080e3a05e4bfed8ac0bc258
Carrie Liang <20.c.liang@gmail.com>
2016-08-17 10:37:32 -0700

    create empty Xcode project

---
+++ invisiblethread/invisiblethread/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  invisiblethread
+//
+//  Created by Carrie Liang on 8/17/16.
+//  Copyright © 2016 Carrie Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ invisiblethread/invisiblethread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  invisiblethread
+//
+//  Created by Carrie Liang on 8/17/16.
+//  Copyright © 2016 Carrie Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
